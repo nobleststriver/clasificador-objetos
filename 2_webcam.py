03f57265a6799f24b59bf401d6595e4473304f85
nobleststriver <nobleststriver@icloud.com>
2026-05-29 02:34:59 -0700

    inferencia webcam 1:1
    cambié la relación de aspecto para que sea un cuadrado perfecto, pensando que serviría de algo, pero aun así está medio menso el modelo

--- 2_webcam.py
+++ 2_webcam.py
@@ -14,47 +14,49 @@ print("cámara encendida. presiona la tecla 'q' en tu teclado para salir.")
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("error al leer la cámara.")
+        print("error al leer la camara.")
         break
 
-    # 1. preprocesamiento
-    # copia reducida a 224x224
-    imagen_ia = cv2.resize(frame, (224, 224))
+    # dimensiones del frame original
+    alto, ancho, _ = frame.shape
     
-    # opencv captura en formato bgr y habíamos entrenado con rgb
+    # lado del cuadrado = dimension más corta (usualmente el alto)
+    lado = min(alto, ancho)
+    
+    # calculamos las coordenadas para centrar el cuadrado
+    x_inicio = (ancho // 2) - (lado // 2)
+    y_inicio = (alto // 2) - (lado // 2)
+    
+    # recorte de frame usando slicing de matrices en numpy
+    frame_cuadrado = frame[y_inicio:y_inicio+lado, x_inicio:x_inicio+lado]
+    # ----------------------------------------------
+
+    # 1. preprocesamiento (ahora sobre el cuadrado perfecto, sin deformar la geometría)
+    imagen_ia = cv2.resize(frame_cuadrado, (224, 224))
     imagen_ia = cv2.cvtColor(imagen_ia, cv2.COLOR_BGR2RGB)
-    
-    # convertimos la imagen a un tensor agregando la dimensión del lote (batch)
-    # (224, 224, 3) a (1, 224, 224, 3)
     imagen_ia = np.expand_dims(imagen_ia, axis=0)
 
     # 2. inferencia
     prediccion = modelo.predict(imagen_ia, verbose=0)
-    
-    # extraemos el índice con el valor más alto y su porcentaje de seguridad
     indice_clase = np.argmax(prediccion)
     confianza = np.max(prediccion) * 100
     etiqueta = clases[indice_clase]
 
-    # 3. visualización en pantalla
-    # rectángulo negro semitransparente arriba para que el texto se lea bien
-    cv2.rectangle(frame, (0, 0), (640, 40), (0, 0, 0), -1)
+    # 3. visualización
+    # ajustamos el rectángulo negro al ancho del nuevo frame cuadrado
+    cv2.rectangle(frame_cuadrado, (0, 0), (lado, 40), (0, 0, 0), -1)
     
     texto = f"{etiqueta}: {confianza:.2f}%"
-    
-    # > 70% segura, texto verde
-    # si duda, texto rojo
     color_texto = (0, 255, 0) if confianza > 70 else (0, 0, 255)
     
-    cv2.putText(frame, texto, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, color_texto, 2)
+    # texto sobre el frame cuadrado
+    cv2.putText(frame_cuadrado, texto, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color_texto, 2)
 
-    cv2.imshow('clasificador en tiempo real', frame)
+    # únicamente ventana recortada
+    cv2.imshow('clasificador en tiempo real', frame_cuadrado)
 
-    # 4. condición de salida
-    # 'q' rompe el bucle
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# limpieza de memoria y apagado del hardware
 cap.release()
 cv2.destroyAllWindows()
